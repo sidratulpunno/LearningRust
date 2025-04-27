ac71b7f2172075bfb8536facbec9e61ae1a956a0
sidratulpunno <sidratulmuntahapunno845000.com>
2025-04-27 23:13:15 +0600

    match funtion

---
+++ src/bin/cwt_match.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let my_name="puno";
+    match my_name{
+        "punno"=>println!("hello punno"),
+        "punnu"=>println!("hello punnu"),
+        _=>println!("good bye")
+    }
+}
